f5ef529404ec14daf7db3239c05378fa47ddba50
Kasey Helm <khelm92@fullsail.edu>
2014-12-04 16:37:23 -0800

    finished story section of madlib

---
+++ madlib/madlib.py
@@ -0,0 +1,5 @@
+story = ''' When a young {gender} named Little {name} was in class one day, the teacher asked Little {name} "if there was anywhere in the world you could go at anytime, where would it be?"
+and Little {name} answered "Well I would go to {location} in {year}!", and instantly Little {name} was teleported there magically! Only to find out that the spell misfired, and the world was
+filled with horrible {monster_1} and terrible {monster_2}. Next, a genie showed up, and said "I will get you out of here, but you must slay {number_1} of {monster_1} and capture {number_2} of
+{monster_2}". So Little {name} set off on the arduous quest. Unbeknownst to Little {name}, the devious genie had his fingers cross behind his back. When Little {name} returned, the genie had
+disappeared, and Little {name} was doomed to live in this magical world forever. '''
