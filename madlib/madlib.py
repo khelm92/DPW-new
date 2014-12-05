04a3d687d925606cde0eafacbaeb1d6a4004288a
Kasey Helm <khelm92@fullsail.edu>
2014-12-04 16:43:52 -0800

    added two raw inputs to collect a string

--- madlib/madlib.py
+++ madlib/madlib.py
@@ -1,3 +1,16 @@
+print "Answer the questions to unlock a magical and devious story"
+
+name = raw_input("What is your name?")
+gender = raw_input("Are you a boy or a girl?")
+
+
+
+
+
+
+
+
+
 story = ''' When a young {gender} named Little {name} was in class one day, the teacher asked Little {name} "if there was anywhere in the world you could go at anytime, where would it be?"
 and Little {name} answered "Well I would go to {location} in {year}!", and instantly Little {name} was teleported there magically! Only to find out that the spell misfired, and the world was
 filled with horrible {monster_1} and terrible {monster_2}. Next, a genie showed up, and said "I will get you out of here, but you must slay {number_1} of {monster_1} and capture {number_2} of
