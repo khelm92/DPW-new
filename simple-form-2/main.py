ae4cfa4ba7212a5aaed19481649094f6952595b5
Kasey Helm <khelm92@fullsail.edu>
2014-12-19 20:40:43 -0800

    added all html and finished form

--- simple-form-2/main.py
+++ simple-form-2/main.py
@@ -1,9 +1,55 @@
+#kasey helm; simple form assignment
 
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        page_head = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form Assignment</title>
+    </head>
+    <body>'''
+    
+        page_body = '''<h1>Grocery Store Application</h1>
+        <form method="GET" action="">
+            <label>Name: </label><input type="text" name="user"/>
+            <br>
+            <label>Email: </label><input type="text" name="email"/>
+            <br>
+            <label>Age: </label><input type="text" name="age"/>
+            <br>
+            <label>Position: </label> <br>
+                                      <input type="radio" name="position" value="cashier" checked>Cashier
+                                      <br>
+                                      <input type="radio" name="position" value="bagger" checked>Bagger
+                                      <br>
+                                      <input type="radio" name="position" value="stock clerk" checked>Stock Clerk
+                                      <br>
+            <label>Location(s): </label> <br>
+                                         <input type="checkbox" name="location" value="san diego">San Diego
+                                         <br>
+                                         <input type="checkbox" name="location" value="la jolla">La Jolla
+                                         <br>
+                                         <input type="checkbox" name="location" value="el cajon">El Cajon
+                                         <br>
+                                         <input type="checkbox" name="location" value="clairemont">Clairemont
+                                         <br>
+            <input type="submit" value="Submit" />'''
+            
+        page_close = '''</form>
+    </body>
+</html>'''
+
+        if self.request.GET:
+            user = self.request.GET['user']
+            email = self.request.GET['email']
+            age = self.request.GET['age']
+            position = self.request.GET['position']
+            location = self.request.GET['location']
+            self.response.write(page_head + user + ' ' + email + ' ' + age + ' ' + position + ' ' + location + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close)
         
         
 
