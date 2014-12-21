b47ef51df72cf0a835cb5f6f0e0bdca4f8de7ca9
Kasey Helm <khelm92@fullsail.edu>
2014-12-21 14:40:19 -0800

    imported view class from pages file, added input form

--- reusable-library-new/main.py
+++ reusable-library-new/main.py
@@ -1,10 +1,14 @@
-
+#kasey helm - reusable library
+#my app is as if I was a personaly trainer, and clients will be inputting their info including name, gender, age, and weight
 import webapp2
+from pages import View
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
         
+        p = View()
+        
+        self.response.write(p.print_out())
         
 
 app = webapp2.WSGIApplication([
--- reusable-library-new/pages.py
+++ reusable-library-new/pages.py
@@ -1,2 +1,36 @@
-#!/usr/bin/env python
+class View(object):
+    def __init__(self):
+        self.head = '''<!DOCTYPE HTML> 
+<html>
+    <head>
+        <title>Reusable Library Assignment</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>'''
+    
+        self.body = '''<h2>Please enter your information for a personal training session:</h2> <br>
+        <form method="GET" action="">
+            <label>Full Name: </label> <input type="text" name="name">
+            <br>
+            <label>Gender: </label> <br>
+                                    <input type="radio" name="gender" value="male" checked>Male
+                                    <br>
+                                    <input type="radio" name="gender" value="female" checked>Female
+                                    <br>
+            <label>Age: </label> <input type="text" name="age">
+            <br>
+            <label>Weight(lbs): </label> <input type="text" name="weight">
+            <br>
+            <input type="submit" value="Submit" id="submit" />
+        '''
+        self.close = '''
+        </form>
+    </body>
+</html>'''
+
+    def print_out(self):
+        all = self.head + self.body + self.close
+        return all
+    
+
 
